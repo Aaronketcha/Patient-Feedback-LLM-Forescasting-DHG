1cac0a3ded949b5ef62ad56dc3e1eaa658d5190d
samuel1544 <samuel.charlessamuel03@gmail.com>
2025-07-30 02:39:08 +0000

    retry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import logging
 
@@ -10,22 +9,6 @@ from crud import load_medications_data
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Créer l'instance de l'application
-app = FastAPI(lifespan=lifespan)
-
-# Configuration du middleware CORS
-origins = [
-    "http://localhost",  # Pour les tests locaux
-    "*"  # Autorise tous les domaines (utilisez avec précaution en production)
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,  # Domaines autorisés
-    allow_credentials=True,
-    allow_methods=["*"],  # Autorise toutes les méthodes (GET, POST, etc.)
-    allow_headers=["*"],  # Autorise tous les en-têtes
-)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -36,20 +19,22 @@ async def lifespan(app: FastAPI):
     logging.info("Application startup event: Loading medications and starting scheduler.")
     
     medications_data = load_medications_data()
-    app.state.medications = medications_data  # Stocke les médicaments dans l'état de l'application
+    app.state.medications = medications_data # Stocke les médicaments dans l'état de l'application
 
     if not app.state.medications:
         logging.warning("No medication data loaded. Check medications.json file.")
 
     scheduler.start()
-    load_medications_into_scheduler(app.state.medications)  # Charge les médicaments dans le scheduler
+    load_medications_into_scheduler(app.state.medications) # Charge les médicaments dans le scheduler
     logging.info("Scheduler started and initial reminders scheduled.")
 
-    yield  # L'application est en cours d'exécution
+    yield # L'application est en cours d'exécution
 
     logging.info("Application shutdown event: Shutting down scheduler.")
     scheduler.shutdown()
     logging.info("Scheduler shut down.")
+
+app = FastAPI(lifespan=lifespan)
 
 # Inclure les routeurs de l'API
 app.include_router(medications.router)
